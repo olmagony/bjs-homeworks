d3ceee3ebea0708642529c4b8592b46fb5ec2e99
olmagony <olmagony@gmail.com>
2020-01-20 14:14:29 +0500

    first commit variables

---
+++ 1.1-variables/main.js
@@ -0,0 +1,25 @@
+console.log('===Задача 1===');
+let algebra = 5;
+let geography = 4;
+let physics = 4;
+
+let averageRating = (algebra + geography + physics) / 3;
+
+console.log(averageRating);
+console.log();
+
+console.log('===Задача 2===');
+
+let myName = `Nata`;
+let message = `Привет, мир! Меня зовут ${myName}`;
+console.log(message);
+console.log();
+
+console.log('===Задача 3===');
+let x = 2;
+let y = 22;
+let z = 0;
+let result = x * y + 5 * z + x - 1;
+console.log(result);
+
+                          
